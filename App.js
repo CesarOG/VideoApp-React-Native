43f5f4ee2229da8dfb06d8275804af78349eccd9
mcortega <ortegapb@gmail.com>
2019-05-28 23:52:05 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Text } from "react-native";
+import Home from "./src/screens/containers/home";
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <Home>
+        <Text>header</Text>
+        <Text>buscador</Text>
+        <Text>categorías</Text>
+        <Text>sugerencias</Text>
+      </Home>
+    );
+  }
+}
